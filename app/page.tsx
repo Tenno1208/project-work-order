3629dba1a74d5bea4ebb94f90cc887b7c577e0fc
tenno <tes81925@gmail.com>
2026-01-15 08:37:53 +0700

    fix redirect login page tsx use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-import Image from "next/image";
-import { redirect } from "next/navigation";
+"use client";
 
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  redirect("/login");
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/login");
+  }, []);
+
+  return null;
 }
